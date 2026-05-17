f13f0e64d37649ac800645941d0e8f6226ada958
MihailPy <mihailpy02@gmail.com>
2026-05-17 17:31:57 +0300

    Add application config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/MihailPy/quartet-game/internal/config"
 )
 
 func main() {
-	addr := ":8080"
+	cfg := config.Load()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("OK"))
 	})
-	log.Printf("Quartet Game API is running on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	log.Printf("Quartet Game API is running on %s", cfg.HTTPAddr)
+	if err := http.ListenAndServe(cfg.HTTPAddr, mux); err != nil {
 		log.Fatal(err)
 	}
 }
