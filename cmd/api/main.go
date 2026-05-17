5709e41061c59f728568abbe42cb45240bf05b2f
MihailPy <mihailpy02@gmail.com>
2026-05-17 17:26:34 +0300

    Add basic HTTP server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Quartet Game API")
+	addr := ":8080"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+	log.Printf("Quartet Game API is running on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
 }
