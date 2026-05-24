b5b89c66afe4f1207873cf93516f359f9c1ebf42
MihailPy <mihailpy02@gmail.com>
2026-05-24 10:34:24 +0300

    Load default deck from database

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,14 +3,16 @@ package config
 import "os"
 
 type Config struct {
-	HTTPAddr    string
-	DatabaseURL string
+	HTTPAddr      string
+	DatabaseURL   string
+	DefaultDeckID string
 }
 
 func Load() Config {
 	return Config{
-		HTTPAddr:    getEnv("HTTP_ADDR", ":8080"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgres://quartet_user:quartet_password@localhost:5432/quartet_game?sslmode=disable"),
+		HTTPAddr:      getEnv("HTTP_ADDR", ":8080"),
+		DatabaseURL:   getEnv("DATABASE_URL", "postgres://quartet_user:quartet_password@localhost:5432/quartet_game?sslmode=disable"),
+		DefaultDeckID: getEnv("DEFAULT_DECK_ID", "00000000-0000-0000-0000-000000000001"),
 	}
 }
 
