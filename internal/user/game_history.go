de5c1dd8f74ecce109f17070db463c6bdaca9120
MihailPy <mihailpy02@gmail.com>
2026-06-25 10:19:33 +0300

    Add winner score to game history

--- internal/user/game_history.go
+++ internal/user/game_history.go
@@ -3,12 +3,13 @@ package user
 import "time"
 
 type GameHistoryRecord struct {
-	ID        string    `json:"id"`
-	GameID    string    `json:"game_id"`
-	RoomID    string    `json:"room_id"`
-	UserID    UserID    `json:"user_id"`
-	Role      string    `json:"role"`
-	Score     int       `json:"score"`
-	IsWinner  bool      `json:"is_winner"`
-	CreatedAt time.Time `json:"created_at"`
+	ID          string    `json:"id"`
+	GameID      string    `json:"game_id"`
+	RoomID      string    `json:"room_id"`
+	UserID      UserID    `json:"user_id"`
+	Role        string    `json:"role"`
+	Score       int       `json:"score"`
+	WinnerScore int       `json:"winner_score"`
+	IsWinner    bool      `json:"is_winner"`
+	CreatedAt   time.Time `json:"created_at"`
 }
